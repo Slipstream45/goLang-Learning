05082a92b5dfca4b5393c37eb022afa6c201063a
Anirban Chakraborty <55631460+Slipstream45@users.noreply.github.com>
2023-05-22 18:35:27 +0530

    Update prog2.go

--- prog2.go
+++ prog2.go
@@ -13,3 +13,22 @@ func main() {
 	fmt.Println("Some jerk ate", eatenCount, "apples.")
 	fmt.Println("Now I am left with", originalCount-eatenCount, "apples.")
 }
+
+//can also do following
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	originalCount := 10
+	eatenCount := 4
+
+	fmt.Println("I started with", originalCount, "apples.")
+	fmt.Println("Some jerk ate", eatenCount, "apples.")
+	fmt.Println("Now I am left with", originalCount-eatenCount, "apples.")
+}
+
