d0c39ea578875555cc22bed0f5704d24ab61eb3c
Anirban Chakraborty <55631460+Slipstream45@users.noreply.github.com>
2023-05-23 09:20:17 +0530

    playing with multiple returns on functions

--- prog6.go
+++ prog6.go
@@ -14,3 +14,28 @@ func floatParts(number float64) (integerPart int, fractionalPart float64) {
 	wholeNumber := math.Floor(number)
 	return int(wholeNumber), number - wholeNumber
 }
+
+
+//another one
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	amount, err := paintNeeded(4.2, -3.0)
+	if err == nil {
+		fmt.Printf("%0.2f litres required!", amount)
+	} else {
+		fmt.Println(err)
+	}
+}
+func paintNeeded(length float64, breadth float64) (float64, error) {
+	if length < 0 || breadth < 0 {
+		return 0, fmt.Errorf("Invalid parameters given!!")
+	}
+	area := length * breadth
+	return area, nil
+}
